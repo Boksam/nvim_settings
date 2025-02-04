eaf13f99a208e7afd23d28f24d56f9d861cb2dda
Park Junwoo <boksam1017@gmail.com>
2025-02-03 22:21:03 -0800

    transparent theme

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 require("junwoo.core")
 require("junwoo.lazy")
+
+vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi NvimTreeNormalNC guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE]])
